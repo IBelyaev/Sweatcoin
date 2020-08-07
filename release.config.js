9ff7ac04a3d3a1fe362d47e9e65c780b29316e26
Igor Belyaev <IYBelyaev@alfabank.ru>
2020-08-07 17:46:25 +0300

    some url

--- release.config.js
+++ release.config.js
@@ -3,9 +3,14 @@ module.exports = {
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
+        [
+            '@semantic-release/npm',
+            { pkgRoot: './.publish' }
+        ],
         '@semantic-release/git'
     ],
     branches: [
         'master'
     ],
+    repositoryUrl: 'https://github.com/IBelyaev/Sweatcoin'
 };
